e535a48f66e8cbc5ef2f56f4411f6083aeb1f22a
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-01-18 22:33:34 -0300

    Criação de algumas pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './component/Header';
 
 function App() {
   return (
-    <div className='bg-red-500'>
+    <div>
       <Header/>
+      <main className='pt-16 bg-slate-100 min-h-[calc(100vh)]'>
+        <Outlet/>
+      </main>
     </div>
   );
 }

c172e87e5156031f84f34924dbfd2df79ac130d2
RodrigoMarquesPF <rodrigomarquespf@gmail.com>
2024-01-23 21:53:53 -0300

    img produto

--- frontend/src/page/Newproduct.js
+++ frontend/src/page/Newproduct.js
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsCloudUpload } from "react-icons/bs";
+import { ImagetoBase64 } from '../utility/imagetoBase64';
 
 const Newproduct = () => {
+
+  const [data,setData] = useState({
+    name:"",
+    category:"",
+    image:"",
+    price:"",
+    description:""
+  })
+
+  const handleOnChange= (e)=>{
+    const {name, value} = e.target
+
+    setData((preve)=>{
+        return{
+          ...preve,
+          [name] :value
+        }
+    })
+  }
+
+  const uploadImage =async(e)=>{
+    const data = await ImagetoBase64(e.target.files[0]);
+    //console.log(data);
+    setData((preve)=>{
+      return{
+        ...preve,
+        image : data
+      }
+    })
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log(data)
+  }
+
   return (
     <div className='p-4'>
-      <form className='m-auto w-full max-w-md  shadow flex flex-col p-3 bg-white'>
+      <form className='m-auto w-full max-w-md  shadow flex flex-col p-3 bg-white' onSubmit={handleSubmit}>
         <label htmlFor='name'>Name</label>
-        <input type={"text"} name="name" className='bg-slate-200 p-1 my-1'/>
+        <input type={"text"} name="name" className='bg-slate-200 p-1 my-1' onChange={handleOnChange}/>
 
         <label htmlFor='category'>Category</label>
-        <select className='bg-slate-200 p-1 my-1' id='category'>
+        <select className='bg-slate-200 p-1 my-1' id='category' name='category' onChange={handleOnChange}>
           <option>Fruits</option>
           <option>Vegetable</option>
           <option>Icream</option>
@@ -17,17 +54,22 @@ const Newproduct = () => {
           <option>Pizza</option>
         </select>
 
-        <label htmlFor='image'></label>
-        <div id='image' className='h-40 w-full bg-slate-200 rounded flex items-center justify-center cursor-pointer'> 
-          <span className='text-5xl'><BsCloudUpload /></span>
+        <label htmlFor='image'>Image
+        <div  className='h-40 w-full bg-slate-200 rounded flex items-center justify-center cursor-pointer'>
+          {
+            data.image ? <img src={data.image} className="h-full"/> : <span className='text-5xl'><BsCloudUpload /></span>
+          
+          } 
+          
+          <input type={"file"} accept="image/*" id='image' onChange={uploadImage} className='hidden'/>
         </div>
-
+        </label>
         <label htmlFor='price' className='my-1'>Price</label>
-        <input type={"text"} className='bg-slate-200 p-1 my-1'/>
+        <input type={"text"} className='bg-slate-200 p-1 my-1' name='price' onChange={handleOnChange}/>
       
 
         <label htmlFor='description'>Description</label>
-        <textarea rows={2} className='bg-slate-200 p-1 my-1 resize-none' name='description' ></textarea>
+        <textarea rows={2} className='bg-slate-200 p-1 my-1 resize-none' name='description' onChange={handleOnChange}></textarea>
 
         <button className='bg-red-500 hover:bg-red-600 text-white text-lg font-medium my-2 drop-shadow'>Save</button>
       </form>
